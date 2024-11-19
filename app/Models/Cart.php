eab826fb45a47e4f6c6379add2e87ec099b834ee
nhathdtk <nhat0x9@gmail.com>
2024-11-19 13:38:22 +0700

    feat: cart api

---
+++ app/Models/Cart.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'tab_id', 'meta'];
+
+    public function tab()
+    {
+        return $this->belongsTo(Tab::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
