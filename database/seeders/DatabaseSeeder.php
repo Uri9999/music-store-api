4c51a4ffdd4752bda24822142e765aa3c1fc8c54
nhathdtk <nhat0x9@gmail.com>
2024-11-19 08:33:45 +0700

    feat: seed subscription

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(SubscriptionSeeder::class);
+        
         $this->call(CategorySeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
---
+++ database/seeders/SubscriptionSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Subscription;
+
+class SubscriptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Subscription::updateOrCreate(
+            ['name' => '1 tháng'],
+            [
+
+                'duration_in_days' => 30,
+                'price' => 20000,
+            ]
+        );
+
+        Subscription::updateOrCreate(
+            ['name' => '3 tháng'],
+            [
+                'duration_in_days' => 60,
+                'price' => 30000,
+            ]
+        );
+
+        Subscription::updateOrCreate(
+            ['name' => '6 tháng'],
+            [
+                'duration_in_days' => 90,
+                'price' => 50000,
+            ]
+        );
+    }
+}
