b04624838df362d9c9e2f409f0887b5665cd4484
nhathdtk <nhat0x9@gmail.com>
2024-11-19 08:27:05 +0700

    feat: api subscription

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('api')->group(function () {
     Route::get('selections', [SelectionController::class, 'index']);
 
     // Category
-    Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    // Route::get('categories', [CategoryController::class, 'index']);
+    // Route::get('categories/{id}', [CategoryController::class, 'show']);
 
     // Tab
     Route::get('tabs', [TabController::class, 'index']);
