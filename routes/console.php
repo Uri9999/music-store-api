5dd93507027ca9f22d8de0d32dff248de6aa518f
nhathdtk <nhat0x9@gmail.com>
2025-01-08 10:32:00 +0700

    feed: test ci cd

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command(SummaryRevenue::class, [])->monthlyOn(1, '01:00');
+// Schedule::command(TestCommand::class, [])->hourly();
